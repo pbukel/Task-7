2ae1437a26627d3eb44ee3a7d72623aa0089e44b
pbukel <pavlik.b@gmail.com>
2022-05-27 09:59:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  marke: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
+};
+
+console.log(showObjectKeys(audi));
+
+function showObjectKeys(object) {
+  return Object.keys(object);
+}
